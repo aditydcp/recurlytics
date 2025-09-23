e781b26e954cc82849387e0a49bb212742c61285
Aditya Cahyo <aditydcp@gmail.com>
2025-09-23 16:16:16 +0700

    update user avatar
    ensure fallback

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -4,7 +4,7 @@ import EventAnalyticsComponent from "@/components/feature/event-analytics";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { getNameInitials } from "@/lib/nameInitials";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { LogOut, Info } from "lucide-react";
+import { LogOut, Info, User } from "lucide-react";
 import ControlPanelComponent from "@/components/feature/ControlPanelComponent";
 
 export default function DashboardPage() {
@@ -17,12 +17,12 @@ export default function DashboardPage() {
         <div className="flex items-center space-x-4">
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              {user && (
-                <Avatar className="cursor-pointer hover:opacity-80 transition">
-                  <AvatarImage src={user.picture} />
-                  <AvatarFallback>{getNameInitials(user.name)}</AvatarFallback>
-                </Avatar>
-              )}
+              <Avatar className="cursor-pointer hover:opacity-80 transition">
+                <AvatarImage src={user?.picture} />
+                <AvatarFallback>
+                  {user?.name ? getNameInitials(user.name) : (<User className="h-4 w-4" />)}
+                </AvatarFallback>
+              </Avatar>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end" className="mt-2 p-2 min-w-40 space-y-2">
               <DropdownMenuLabel className="px-2 py-1.5 text-xs text-end font-normal text-muted-foreground">
