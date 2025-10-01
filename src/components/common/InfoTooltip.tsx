127f06f991895413c90ab7ec3a029a968cd3a71a
Aditya Cahyo <aditydcp@gmail.com>
2025-10-01 09:37:13 +0700

    update tooltip component

--- src/components/common/InfoTooltip.tsx
+++ src/components/common/InfoTooltip.tsx
@@ -1,16 +1,26 @@
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip"
+import { cn } from "@/lib/utils"
 import { Info } from "lucide-react"
 import { type ReactNode } from "react"
 
 interface InfoTooltipProps {
+  className?: string
   children: ReactNode
 }
 
-export function InfoTooltip({ children }: InfoTooltipProps) {
+export function InfoTooltip({
+  className,
+  children
+}: InfoTooltipProps) {
   return (
     <Tooltip>
       <TooltipTrigger asChild>
-        <Info className="opacity-50 w-4 h-4 hover:text-primary hover:bg-accent rounded-full cursor-pointer" />
+        <Info
+          className={cn(
+            "opacity-50 w-4 h-4 hover:text-primary hover:bg-accent rounded-full cursor-pointer",
+            className
+          )}
+        />
       </TooltipTrigger>
       <TooltipContent>
         <p>{children}</p>
